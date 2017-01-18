41362b2da7293015c328b643354da44de93797dd
Buck Baskin <buckbaskin@google.com>
2017-01-18 12:22:29 -0500

    Lots of updates/reorg

--- src/stopsign_core.py
+++ src/stopsign_core.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 
 from collections import namedtuple
-from sensor_msgs.msg import Image
-
 import cv2
 
 Reading = namedtuple('Reading', ['bearing', 'size', 'r', 'g', 'b', 'a'])
 
-class StopSignFinder(object):
+class StopsignFinder(object):
     def __init__(self):
         self.cap = cv2.VideoCapture(0) # create video capture object
 
-    def check_for_stopsign(self, unwrap=True):
-        img = self.retrieve_image()
+    def check_for_stopsign(self, unwrap=True, img=None):
+        if img is None:
+            img = self.retrieve_image()
         if unwrap:
             img = self.unwrap_image(img)
         readings = self.panorama_to_readings(img)
         return self.has_stopsign(readings)
 
     def retrieve_image(self):
-        cvImage = self.cap.read()
-        return cvImage
+        cv_image = self.cap.read()
+        return cv_image
 
     def unwrap_image(self, donut):
         # TODO(buckbaskin): take a donut image and convert it to an unwrapped 360
@@ -31,7 +30,7 @@ class StopSignFinder(object):
         # TODO(buckbaskin): given an unwrapped panorama, find blobs using opencv and
         #   convert their position in the image to a reading (bearing, size, color)
         list_of_readings = []
-        list_of_readings.append(Reading(0,1,0,0,0,0))
+        list_of_readings.append(Reading(0, 1, 0, 0, 0, 0))
         return list_of_readings
 
     def has_stopsign(self, readings):
--- src/stopsign_ros.py
+++ src/stopsign_ros.py
@@ -1,25 +1,18 @@
 import cv2
-import stopsign_core
 import rospy
 
+from stopsign_core import StopsignFinder
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
-from viz_feature_sim.msg import VizScan, Blob
+from std_msgs.msg import Bool
 
-bearing_out = rospy.Publisher('processed_image/bearings', VizScan, queue_size=1)
-bridge = CvBridge()
+finder = StopsignFinder()
 
-def core_to_rosmsg(reading_list):
-    temp = []
-    for reading in reading_list:
-        blob = Blob()
-        blob.bearing = reading.bearing
-        blob.size = reading.size
-        blob.color.r = reading.r
-        blob.color.g = reading.g
-        blob.color.b = reading.b
-        temp.append(blob)
-    return temp
+out = rospy.Publisher('stopsign', Bool, queue_size=1)
+
+def active_image():
+    mess = Bool()
+    mess.data = finder.has_stopsign()
+    out.publish(mess)
 
 def image_cb(image_msg):
     header = image_msg.header
@@ -27,21 +20,22 @@ def image_cb(image_msg):
     w = image_msg.width
     encoding = image_msg.encoding
     is_bigendian = image_msg.is_bigendian
-    uint32 step = image_msg.step
+    uint32_step = image_msg.step
     image_data = image_msg.data
 
     try:
         cv_image = bridge.imgmsg_to_cv2(image_msg, desired_encoding='passthrough')
-        viz_scan = VizScan()
-        bearing_out.publish(core_to_rosmsg(stopsign_core.one_click_process()))
+        mess = Bool()
+        mess.data = finder.has_stopsign(img=cv_image)
+        out.publish(mess)
     except CvBridgeError as cvbe:
-        print cvbe
-        return None
-
-
+        print(cvbe)
+    
+    return None
 
 image_in = rospy.Subscriber('/camera/image/rgb', Image, image_cb)
 
 if __name__ == '__main__':
     rospy.init_node('stopsign')
-    rospy.spin()
+    while not rospy.is_shutdown():
+        active_image()
