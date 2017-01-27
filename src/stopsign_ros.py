ff982e770b7a866ae2a4e1fcab01f15b8449c785
Buck Baskin <buckbaskin@google.com>
2017-01-27 13:20:42 -0500

    Fix some errors identified by lint

--- src/stopsign_ros.py
+++ src/stopsign_ros.py
@@ -1,42 +1,45 @@
 import cv2
 import rospy
 
-from stopsign_core import StopsignFinder
 from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
 from std_msgs.msg import Bool
+from stopsign_core import StopsignFinder
 
-finder = StopsignFinder()
+FINDER = StopsignFinder()
 
 OUT = rospy.Publisher('stopsign', Bool, queue_size=1)
 
 CAP = cv2.VideoCapture(0)
 
+BRIDGE = CvBridge()
+
 def active_image():
     cv_image = CAP.read()
     mess = Bool()
-    mess.data = finder.check_for_stopsign(unwrap=False, img=cv_image, debug=False, save=False)
+    mess.data = FINDER.check_for_stopsign(unwrap=False, img=cv_image, debug=False, save=False)
     OUT.publish(mess)
 
 def image_cb(image_msg):
-    header = image_msg.header
-    h = image_msg.height
-    w = image_msg.width
-    encoding = image_msg.encoding
-    is_bigendian = image_msg.is_bigendian
-    uint32_step = image_msg.step
-    image_data = image_msg.data
+    # header = image_msg.header
+    # h = image_msg.height
+    # w = image_msg.width
+    # encoding = image_msg.encoding
+    # is_bigendian = image_msg.is_bigendian
+    # uint32_step = image_msg.step
+    # image_data = image_msg.data
 
     try:
-        cv_image = bridge.imgmsg_to_cv2(image_msg, desired_encoding='passthrough')
+        cv_image = BRIDGE.imgmsg_to_cv2(image_msg, desired_encoding='passthrough')
         mess = Bool()
-        mess.data = finder.check_for_stopsign(unwrap=False, img=cv_image, debug=False, save=False)
+        mess.data = FINDER.check_for_stopsign(unwrap=False, img=cv_image, debug=False, save=False)
         OUT.publish(mess)
     except CvBridgeError as cvbe:
         print(cvbe)
     
     return None
 
-image_in = rospy.Subscriber('/camera/image/rgb', Image, image_cb)
+IMAGE_IN = rospy.Subscriber('/camera/image/rgb', Image, image_cb)
 
 if __name__ == '__main__':
     rospy.init_node('stopsign')
