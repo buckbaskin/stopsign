b342891cdd89a9f79a113f96a8027503115a9167
Buck Baskin <mobile.wbaskin@gmail.com>
2017-12-07 12:38:48 -0500

    Ready to run?

--- src/classify_image.py
+++ src/classify_image.py
@@ -29,6 +29,9 @@ end_image_id = 2189
 
 IMAGE_BASE_STRING = '%s/data/002_original_images/%s' % (pkg_path, 'frame%04d.jpg')
 
+NUM_IMAGES = end_image_id - start_image_id
+NUM_ORBS_FEATURES = 500
+
 descriptors = []
 for i in range(32):
     descriptors.append('descr%02d' % (i,))
@@ -54,13 +57,17 @@ def load_data(seed=None):
     # use mask to split into test, train
     if seed is not None:
         np.random.seed(seed)
-    msk = np.random.rand(len(df)) < 0.8
-    train_X = X[msk].as_matrix()
-    test_X = X[~msk].as_matrix()
-    train_y = y[msk][:,0].as_matrix().ravel()
-    test_y = y[~msk][:,0].as_matrix().ravel()
-    train_id = y[msk][:,1].as_matrix().ravel()
-    test_id = y[~msk][:,1].as_matrix().ravel()
+    img_msk = np.random.rand(NUM_IMAGES) < 0.8
+    X['train'] = X['imageid'].apply(lambda x: img_msk[x])
+    X_msk = X['train'] == 1
+    y['train'] = y['imageid'].apply(lambda x: img_msk[x])
+    y_msk = y['train'] == 1
+    train_X = X[X_msk].as_matrix()
+    test_X = X[~X_msk].as_matrix()
+    train_y = y[y_msk][:,0].as_matrix().ravel()
+    test_y = y[~y_msk][:,0].as_matrix().ravel()
+    train_id = y[y_msk][:,1].as_matrix().ravel()
+    test_id = y[~y_msk][:,1].as_matrix().ravel()
     return train_X, train_y, train_id, test_X, test_y, test_id
 
 def subsample_data(X, y, ratio=0.5, seed=None):
@@ -88,28 +95,53 @@ if __name__ == '__main__':
     print('begin loading data')
     train_X, train_y, train_id, test_X, test_y, test_id = load_data(12345)
 
+    print('train kp classifier')
     kp_nbrs = KNeighborsClassifier()
     # train_X, train_y = subsample_data(train_X, train_y, ratio=0.5, seed=123456)
     kp_nbrs.fit(train_X, train_y)
 
     # I need to group by images first
 
-    NUM_IMAGES = end_image_id - start_image_id
-    NUM_ORBS_FEATURES = 500
+    image_train_X = np.zeros(len(train_X), NUM_ORBS_FEATURES)
+    image_train_y = np.zeros(len(train_y)).ravel()
+    image_test_X = np.zeros(len(test_X), NUM_ORBS_FEATURES)
+    image_test_y = np.zeros(len(test_y)).ravel()
 
-    image_train_X = np.zeros(NUM_IMAGES, NUM_ORBS_FEATURES)
-    image_train_y = np.zeros(NUM_IMAGES).ravel()
+    itr = 0
+    ite = 0
 
+    print('groupby image')
     # compile images into features (classification of keypoints)
     #   and the actual label for the image
     for i in range(start_image_id, end_image_id):
         mask = train_id == i
         subset_X = train_X[mask]
-        subset_y = train_y[mask]
+        if len(subset_X) > 0:
+            subset_y = train_y[mask]
 
-        predict_y = kp_nbrs.predict(subset_X).flatten()
-        image_train_X[i, :] = predict_y
-        image_train_y = (train_y == 1).any()
+            predict_y = kp_nbrs.predict(subset_X).flatten()
+            image_train_X[itr, :] = predict_y
+            image_train_y[itr] = (train_y == 1).any()
+            itr += 1
 
+        subset_X = test_X[mask]
+        if len(subset_X) > 0:
+            subset_y = test_y[mask]
+
+            image_test_X[ite, :] = subset_y
+            image_test_y[ite] = (subset_y == 1).any()
+            ite += 1
+
+
+    print('train image classifier on groupby image')
     gnb = GaussianNB()
     gnb.fit(image_train_X, image_train_y)
+
+    image_y_pred = gnb.predict(image_test_X)
+
+    print('ready to predictt on test data')
+    print(gnb)
+    print('a: %.4f (percent correctly classified)' % (accuracy_score(y_true=image_test_y, y_pred=image_y_pred),))
+    print('p: %.4f (percent of correct positives)' % (precision_score(y_true=image_test_y, y_pred=image_y_pred),))
+    print('r: %.4f (percent of positive results found)' % (recall_score(y_true=image_test_y, y_pred=image_y_pred),))
+    print('---')
