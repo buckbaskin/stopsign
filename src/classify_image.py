ee18ec59b153b2f07a3b3102705b229b019b24f1
Buck Baskin <mobile.wbaskin@gmail.com>
2017-12-07 21:48:22 -0500

    Save work here. Potentially forking CSV transformation

--- src/classify_image.py
+++ src/classify_image.py
@@ -2,14 +2,15 @@
 import rospkg
 
 import cv2
+import datetime
 import gc
+import joblib
 import numpy as np
 import pandas as pd
 
 from imblearn.under_sampling import RandomUnderSampler
 
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.external import joblib
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
@@ -26,9 +27,9 @@ IMAGE_RATE = 11 # hz
 
 BULK_DATA_FILE = '%s/data/003_manual_labels/all.csv' % (pkg_path,)
 
-IMAGE_DATA_FILE = '%s/data/004_image_labels/all.csv' % (pkg_path,)
+IMAGE_DATA_FILE = '%s/data/005_image_labels/all.csv' % (pkg_path,)
 
-KP_MODEL_STORE_FILE = '%s/data/004_image_labels/kp_classifier.pkl' % (pkg_path,)
+KP_MODEL_STORE_FILE = '%s/data/005_image_labels/kp_classifier.pkl' % (pkg_path,)
 
 start_image_id = 0
 end_image_id = 2189
@@ -58,7 +59,7 @@ def load_data(seed=None):
     # split into class, features
     X = df[descriptors]
     y = df[klass]
-    print(y.describe())
+    # print(y.describe())
 
     # use mask to split into test, train
     if seed is not None:
@@ -68,7 +69,11 @@ def load_data(seed=None):
     X_msk = X['train'] == 1
     y['train'] = y['imageid'].apply(lambda x: img_msk[x])
     y_msk = y['train'] == 1
+
+    X.drop('train', axis=1, inplace=True)
     train_X = X[X_msk].as_matrix()
+    # print('describe train_X')
+    # print(X[X_msk].describe())
     test_X = X[~X_msk].as_matrix()
     train_y = y['class  '][y_msk].as_matrix().ravel()
     test_y = y['class  '][~y_msk].as_matrix().ravel()
@@ -79,15 +84,17 @@ def load_data(seed=None):
 def load_data_by_image(start_id, end_id, seed=12345):
     # lazy load image data?
     df = pd.read_csv(BULK_DATA_FILE, header=0, skiprows=lambda x: 1 <= x <= start_id*500, nrows=500*(end_id - start_id))
-    print(df.describe())
+    # print(df.describe())
 
     gby_imageid = df.groupby(['imageid'])
 
-    for name, indices in gby_imageid:
+    for name, indices in gby_imageid.indices.iteritems():
         subdf = df.iloc[indices]
     # split into class, features
-        X = subdf[descriptors]
-        y = subdf[klass]
+        X = subdf[descriptors].as_matrix()
+        y = subdf[klass].as_matrix().ravel()
+        # print('describe X')
+        # print(subdf[descriptors].describe())
         yield X, y
 
 def subsample_data(X, y, ratio=0.5, seed=None):
@@ -101,16 +108,29 @@ def subsample_data(X, y, ratio=0.5, seed=None):
     return rus.fit_sample(X, y)
 
 def train_and_save_kp_classifier():
+    start_time = datetime.datetime.now()
+    print('Time 0 sec')
     print('begin loading data')
     train_X, train_y, train_id, test_X, test_y, test_id = load_data(seed=12345)
 
-    print('train kp classifier')
-    kp_nbrs = KNeighborsClassifier()
+    print('train kp classifier %.2f sec' % ((datetime.datetime.now() - start_time).total_seconds(),))
+    kp_nbrs = KNeighborsClassifier(n_jobs=1)
     # train_X, train_y = subsample_data(train_X, train_y, ratio=0.5, seed=123456)
+    print('train_X shape')
+    print(train_X.shape)
     kp_nbrs.fit(train_X, train_y)
 
-    print('save trained classifier')
+    print('and now, predict %.2f sec' % ((datetime.datetime.now() - start_time).total_seconds(),))
+
+    # y_pred = kp_nbrs.predict(test_X)
+    print('kp classifier %.2f sec' % ((datetime.datetime.now() - start_time).total_seconds(),))
+    print('a:')
+    # print(accuracy_score(y_true=test_y, y_pred=y_pred))
+
+    print('save trained classifier %.2f sec' % ((datetime.datetime.now() - start_time).total_seconds(),))
     joblib.dump(kp_nbrs, KP_MODEL_STORE_FILE)
+
+    print('saved classifier %.2f sec' % ((datetime.datetime.now() - start_time).total_seconds(),))
 
 def load_kp_and_write_data_by_image():
     kp_nbrs = joblib.load(KP_MODEL_STORE_FILE)
@@ -124,18 +144,20 @@ def load_kp_and_write_data_by_image():
             yield ('orbkp%03d' % (i,))
 
     out = pd.DataFrame(columns = columns())
-    outline = np.zeroes((1, NUM_ORBS_FEATURES + 2))
+    outline = np.zeros((1, NUM_ORBS_FEATURES + 2))
     out.to_csv(IMAGE_DATA_FILE)
     
     imageid = 0
     for i in range(0, 4):
         for X, y in load_data_by_image(i * step, i * step + step):
             # X, y grouped by image
+            # print('X shape')
+            # print(X.shape)
             pred_y = kp_nbrs.predict(X)
             outline[0, 0] = (y == 1).any()
-            outline[0, 0] = imageid
+            outline[0, 1] = imageid
             outline[0, 2:] = pred_y.flatten()
-            out.append(outline)
+            out.append(list(outline), columns())
             imageid += 1
 
         with open(IMAGE_DATA_FILE, 'a') as f:
@@ -151,7 +173,7 @@ def load_image_labels_and_classify_images():
     # split into class, features
     X = df[features()]
     y = df['class']
-    print(y.describe())
+    # print(y.describe())
 
     np.random.seed(12345)
     msk = np.random.rand(len(df)) < 0.8
@@ -185,6 +207,8 @@ if __name__ == '__main__':
     '''
     train_and_save_kp_classifier()
     gc.collect()
+
+    print('collect after classifier')
     
     # load data from csv, split into training and test sets
     # classify all keypoints and write classifications back to disk
@@ -192,7 +216,7 @@ if __name__ == '__main__':
     load_kp_and_write_data_by_image()
     gc.collect()
 
-    load_image_labels_and_classify_images()
-    import sys
-    sys.exit(1)
+    print('collect after write by image')
+
+    # load_image_labels_and_classify_images()
     gc.collect()
