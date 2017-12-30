fe40fbdd2b6d8a1bdb9957a3ea9acf346a0f1069
Buck Baskin <mobile.wbaskin@gmail.com>
2017-12-30 02:45:18 -0500

    Modify bitwise_visualize

--- src/visualize/bitwise_visualize.py
+++ src/visualize/bitwise_visualize.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
-df = pd.read_csv(BULK_DATA_FILE, header=0)
+import pandas as pd
+
+pkg_path = '/home/buck/ros_ws/src/stopsign'
+START_FILE = '%s/data/015_visualize/positive_bits_200.csv' % (pkg_path,)
+
+bit_label = 'd%02db%01d'
+
+descriptors = []
+for i in range(32):
+    for bit_index in range(0, 8):
+        descriptors.append('d%02db%01d' % (i, bit_index,))
+
+klass = ['class']
+
+df = pd.read_csv(START_FILE, header=0)
+
+print(df.describe())
+
