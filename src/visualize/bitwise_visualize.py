9c132c1d8c7fed5e325c0de3de5aa3fef0fe8502
Buck Baskin <mobile.wbaskin@gmail.com>
2017-12-30 02:11:49 -0500

    Visualization starting up

---
+++ src/visualize/bitwise_visualize.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+df = pd.read_csv(BULK_DATA_FILE, header=0)
---
+++ src/visualize/split_pos_neg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+pkg_path = '/home/buck/ros_ws/src/stopsign'
+START_FILE = '%s/data/015_visualize/clean_200.csv' % (pkg_path,)
+POSITIVE_FILE = '%s/data/015_visualize/positive_200.csv' % (pkg_path,)
+NEGATIVE_FILE = '%s/data/015_visualize/negative_200.csv' % (pkg_path,)
+
+klass = ['class'.ljust(7)]
+
+df = pd.read_csv(START_FILE, header=0)
+
+neg, pos = df.groupby(by=klass)
+
+nclass_, neg = neg
+pclass_, pos = pos
+
+print(neg.describe())
+import sys
+sys.exit(1)
+
+pos.to_csv(POSITIVE_FILE)
+neg.to_csv(NEGATIVE_FILE)
