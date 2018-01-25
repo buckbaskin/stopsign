2e175407ddde489c114661ad28fc1673a9ad367c
Buck Baskin <mobile.wbaskin@gmail.com>
2018-01-25 12:39:53 -0500

    Made train n save run to completion

--- src/v2/mlp_train_n_save.py
+++ src/v2/mlp_train_n_save.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 
-# TODO(buckbaskin): switch to PCA90 w/ mlp(135,135)
-
 import datetime
 import joblib
 import numpy as np
 import pandas as pd
+import platform
 
+from sklearn.decomposition import PCA, KernelPCA
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import AdaBoostRegressor, BaggingRegressor
 
 pkg_path = '/home/buck/ros_ws/src/stopsign'
+
+FAST_COMPILE = True
 
 IMAGE_RATE = 30 # hz
 
 POSITIVE_BITS_FILE = '%s/data/017_the_500/positive_bits_500.csv' % (pkg_path,)
 NEGATIVE_BITS_FILE = '%s/data/017_the_500/negative_bits_500%s.csv' % (pkg_path, '_%d')
 
-KLASSIFIER_PATH = '%s/data/017_the_500/competition_classifier01.pkl' % (pkg_path,)
+KLASSIFIER_PATH = '%s/data/017_the_500/competition_classifier01_%s.pkl' % (pkg_path, platform.python_version(),)
+REDUCER_PATH = '%s/data/017_the_500/competition_reducer01_%s.pkl' % (pkg_path, platform.python_version(),)
+print('K %s' % KLASSIFIER_PATH)
+print('R %s' % REDUCER_PATH)
 
 bit_label = 'd%02db%01d'
 
@@ -90,7 +96,7 @@ if __name__ == '__main__':
     ### Begin the whole process ###
 
     Klassifiers = [
-        MLPClassifier, 
+        MLPClassifier,
     ]
 
     # experimentally, about 300-320 nodes is the max
@@ -112,95 +118,121 @@ if __name__ == '__main__':
     Klassifier_configs = []
     Klassifier_configs.extend(make_all_combinations(mlp_spec))
 
+    Ensembler = BaggingRegressor
+
     bigX, bigy = load_data(POSITIVE_BITS_FILE, NEGATIVE_BITS_FILE % 0)
 
     split_count = 8
 
-    for num_tests in range(8,9):
-        for index, Klassifier in enumerate(Klassifiers):
-            acc = []
-            train_acc = []
-            pre = []
-            rec = []
-            tim = []
+    try:
+        for num_tests in range(8,9):
+            for index, Klassifier in enumerate(Klassifiers):
+                acc = []
+                train_acc = []
+                pre = []
+                rec = []
+                tim = []
 
-            for config_setup in Klassifier_configs:
-                print('current config: %s' % (config_setup,))
-                acc_accum = 0
-                train_acc_accum = 0
-                pre_accum = 0
-                rec_accum = 0
-                tim_accum = 0
-                for seed in range(0, num_tests):
-                    print('round %4d/%4d' % (seed+1, num_tests))
-                    train_X, train_y, test_X, test_y = scramble_data(bigX, bigy, seed)
-                    
-                    rng = np.random.RandomState(seed+1)
-                    classifier = Klassifier(**config_setup)
-                    print('begin fit')
-                    start = datetime.datetime.now()
-                    classifier.fit(train_X, train_y)
-                    print('end fit | %.1f sec' % ((datetime.datetime.now() - start).total_seconds(),))
-                    y_pred = classifier.predict(train_X)
-                    y_pred = np.where(y_pred > 0.5, 1, 0)
-
-                    train_acc_accum += accuracy_score(y_true=train_y, y_pred=y_pred)
-                    
-                    X_splits = np.array_split(test_X, split_count)
-                    y_splits = np.array_split(test_y, split_count)
-
-                    split_version = list(zip(X_splits, y_splits))
-                    count = 0
-                    for test_X_sub, test_y_sub in split_version:
-                        count += 1
-                        print('sub round %4d/%4d | %.1f sec' % (count, len(split_version), (datetime.datetime.now() - start).total_seconds(),))
-                        stime = datetime.datetime.now()
-                        y_pred = classifier.predict(test_X_sub)
+                for config_setup in Klassifier_configs:
+                    print('current config: %s' % (config_setup,))
+                    acc_accum = 0
+                    train_acc_accum = 0
+                    pre_accum = 0
+                    rec_accum = 0
+                    tim_accum = 0
+                    for seed in range(0, num_tests):
+                        print('round %4d/%4d' % (seed+1, num_tests))
+                        train_X, train_y, test_X, test_y = scramble_data(bigX, bigy, seed)
+                        
+                        rng = np.random.RandomState(seed+1)
+                        # classifier = Ensembler(
+                        #     Klassifier(**config_setup), 
+                        #     random_state=rng,
+                        #     **ensemble_spec)
+                        classifier = Klassifier(**config_setup)
+                        reducer = PCA(n_components=17)
+                        print('begin fit')
+                        start = datetime.datetime.now()
+                        shrink_train_X = reducer.fit_transform(train_X)
+                        print(reducer.explained_variance_ratio_)
+                        classifier.fit(shrink_train_X, train_y)
+                        print('end fit | %.1f sec' % ((datetime.datetime.now() - start).total_seconds(),))
+                        y_pred = classifier.predict(shrink_train_X)
                         y_pred = np.where(y_pred > 0.5, 1, 0)
-                        etime = datetime.datetime.now()
 
-                        acc_accum += accuracy_score(y_true=test_y_sub, y_pred=y_pred)
-                        pre_accum += precision_score(y_true=test_y_sub, y_pred=y_pred)
-                        rec_accum += recall_score(y_true=test_y_sub, y_pred=y_pred)
-                        tim_accum += (etime - stime).total_seconds()
+                        train_acc_accum += accuracy_score(y_true=train_y, y_pred=y_pred)
+                        
+                        if FAST_COMPILE:
+                            print('training accuracy: %.2f' % (train_acc_accum,))
+                            raise ValueError()
 
-                train_acc.append(train_acc_accum / (num_tests))
-                acc.append(acc_accum / (num_tests*split_count))
-                pre.append(pre_accum / (num_tests*split_count))
-                rec.append(rec_accum / (num_tests*split_count))
-                tim.append(tim_accum / (num_tests*split_count))
-                print('a: %.4f (percent correctly classified)' % (acc_accum / (num_tests*split_count),))
-                print('ta:%.4f (percent correctly classified in training)' % (train_acc_accum / num_tests,))
-                print('p: %.4f (percent of correct positives)' % (pre_accum / (num_tests*split_count),))
-                print('r: %.4f (percent of positive results found)' % (rec_accum / (num_tests*split_count),))
-                avg_time = tim_accum / (num_tests*split_count)
-                print('t: %.6f sec' % (avg_time,))
-                if avg_time > (1.0 / 30):
-                    print('stopping based on hard time limit')
-                    break
+                        X_splits = np.array_split(test_X, split_count)
+                        y_splits = np.array_split(test_y, split_count)
 
-            print(Klassifier)
-            print(Ensembler)
-            print('Averaged over %d tests' % (num_tests,))
-            # better accuracy summary
-            print('a: %.4f (avg percent correctly classified)' % (sum(acc)/len(acc),))
-            print('Top Accuracies')
-            print('90 percent of max accuracy cutoff')
-            sorted_ = list(sorted(enumerate(acc), key=lambda x: -x[1]))
-            top_acc = sorted_[0][1]
-            sorted_ = list(filter(lambda x: x[1] >= top_acc * 0.9, sorted_))
-            for acc_index, accuracy in sorted_[:15]:
-                print('% 4.2f | %s' % (accuracy * 100, Klassifier_configs[acc_index],))
-                print('% 4.2f | training accuracy' % (train_acc[acc_index] * 100.0,))
+                        split_version = list(zip(X_splits, y_splits))
+                        count = 0
+                        for test_X_sub, test_y_sub in split_version:
+                            test_X_sub = reducer.transform(test_X_sub)
+                            count += 1
+                            # print('sub round %4d/%4d | %.1f sec' % (count, len(split_version), (datetime.datetime.now() - start).total_seconds(),))
+                            stime = datetime.datetime.now()
+                            y_pred = classifier.predict(test_X_sub)
+                            y_pred = np.where(y_pred > 0.5, 1, 0)
+                            etime = datetime.datetime.now()
 
-            print('p: %.4f (avg percent of correct positives)' % (sum(pre)/len(pre),))
-            print('r: %.4f (avg percent of positive results found)' % (sum(rec)/len(rec),))
+                            acc_accum += accuracy_score(y_true=test_y_sub, y_pred=y_pred)
+                            pre_accum += precision_score(y_true=test_y_sub, y_pred=y_pred)
+                            rec_accum += recall_score(y_true=test_y_sub, y_pred=y_pred)
+                            tim_accum += (etime - stime).total_seconds()
 
-            print('t: %.6f avg sec' % (sum(tim) / len(tim)))
-            print('Top Prediction Latencies')
-            print('Top 10')
-            sorted_ = list(sorted(enumerate(tim), key=lambda x: x[1]))
-            for tim_index, pred_latency in sorted_[:10]:
-                print('%.6f | %s' % (pred_latency, Klassifier_configs[tim_index]))
+                    train_acc.append(train_acc_accum / (num_tests))
+                    acc.append(acc_accum / (num_tests*split_count))
+                    pre.append(pre_accum / (num_tests*split_count))
+                    rec.append(rec_accum / (num_tests*split_count))
+                    tim.append(tim_accum / (num_tests*split_count))
+                    print('a: %.4f (percent correctly classified)' % (acc_accum / (num_tests*split_count),))
+                    print('ta:%.4f (percent correctly classified in training)' % (train_acc_accum / num_tests,))
+                    print('p: %.4f (percent of correct positives)' % (pre_accum / (num_tests*split_count),))
+                    print('r: %.4f (percent of positive results found)' % (rec_accum / (num_tests*split_count),))
+                    avg_time = tim_accum / (num_tests*split_count)
+                    print('t: %.6f sec' % (avg_time,))
+                    if avg_time > (1.0 / 30):
+                        print('stopping based on hard time limit')
+                        break
 
-            # save here
+                print(Klassifier)
+                print(Ensembler)
+                print('Averaged over %d tests' % (num_tests,))
+                # better accuracy summary
+                print('a: %.4f (avg percent correctly classified)' % (sum(acc)/len(acc),))
+                print('Top Accuracies')
+                print('90 percent of max accuracy cutoff')
+                sorted_ = list(sorted(enumerate(acc), key=lambda x: -x[1]))
+                top_acc = sorted_[0][1]
+                sorted_ = list(filter(lambda x: x[1] >= top_acc * 0.9, sorted_))
+                for acc_index, accuracy in sorted_[:15]:
+                    print('% 4.2f | %s' % (accuracy * 100, Klassifier_configs[acc_index],))
+                    print('% 4.2f | training accuracy' % (train_acc[acc_index] * 100.0,))
+
+                print('p: %.4f (avg percent of correct positives)' % (sum(pre)/len(pre),))
+                print('r: %.4f (avg percent of positive results found)' % (sum(rec)/len(rec),))
+
+                print('t: %.6f avg sec' % (sum(tim) / len(tim)))
+                print('Top Prediction Latencies')
+                print('Top 10')
+                sorted_ = list(sorted(enumerate(tim), key=lambda x: x[1]))
+                for tim_index, pred_latency in sorted_[:10]:
+                    print('%.6f | %s' % (pred_latency, Klassifier_configs[tim_index]))
+    except ValueError:
+        print('Fast Compile? %s' % (FAST_COMPILE,))
+        if not FAST_COMPILE:
+            raise
+
+    print(classifier)
+    print(reducer)
+
+    print('I should save these')
+    joblib.dump(classifier, KLASSIFIER_PATH)
+    print('done saving this silly MLP thing')
+    joblib.dump(reducer, REDUCER_PATH)
+    print('done saving this magic PCA thing')
